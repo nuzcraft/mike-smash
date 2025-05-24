7b6839b7b40811d00eacb58e3b07dda1931a5a37
nuzcraft <20135847+nuzcraft@users.noreply.github.com>
2025-05-23 21:57:19 -0500

    working through intro to love tutorial

--- main.lua
+++ main.lua
@@ -2,10 +2,20 @@ if arg[2] == "debug" then
     require("lldebugger").start()
 end
 
+function love.load()
+    x = 100
+end
+
+function love.update(dt)
+    if love.keyboard.isDown("right") then
+        x = x + 100 * dt
+    elseif love.keyboard.isDown("left") then
+        x = x - 100 * dt
+    end
+end
+
 function love.draw()
-    love.graphics.setBackgroundColor(0.5, 0.5, 0.5)
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.print("Hello World!", 400, 300)
+    love.graphics.rectangle("line", x, 50, 200, 150)
 end
 
 local love_errorhandler = love.errorhandler
