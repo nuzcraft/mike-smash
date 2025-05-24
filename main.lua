243602f01698b7e1cfa934b7c20b42875641102d
nuzcraft <20135847+nuzcraft@users.noreply.github.com>
2025-05-24 08:58:28 -0500

    some loops

--- main.lua
+++ main.lua
@@ -4,6 +4,8 @@ end
 
 function love.load()
     x = 100
+    fruits = {"apple", "banana"}
+    table.insert(fruits, "orange")
 end
 
 function love.update(dt)
@@ -16,6 +18,9 @@ end
 
 function love.draw()
     love.graphics.rectangle("line", x, 50, 200, 150)
+    for i, frt in ipairs(fruits) do
+        love.graphics.print(frt, 100, 100 + i * 50)
+    end
 end
 
 local love_errorhandler = love.errorhandler
