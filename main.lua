c4d157829b073cb8f91ac0e7b3b908e05e18ce2c
nuzcraft <20135847+nuzcraft@users.noreply.github.com>
2025-05-26 13:17:47 -0500

    starting on number 14 of the sheepolution book

--- main.lua
+++ main.lua
@@ -1,42 +1,18 @@
+-- require for the debugger
 if arg[2] == "debug" then
     require("lldebugger").start()
 end
 
-local r1, r2
-
 function love.load()
-    tick = require "tick"
     Object = require "classic"
-    local Rectangle = require "rectangle"
-
-    r1 = Rectangle(10, 100, 100, 100)
-    r2 = Rectangle(250, 120, 150, 120)
-
-    myImage = love.graphics.newImage("sheep.png")
-    love.graphics.setBackgroundColor(1, 1, 1)
 end
 
 function love.update(dt)
-    tick.update(dt)
-    r1:update(dt)
-    -- r2:update(dt)
+
 end
 
 function love.draw()
-    local mode
-    if checkCollision(r1, r2) then
-        mode = "fill"
-    else
-        mode = "line"
-    end
-    love.graphics.setColor(1, 0, 0)
-    r1:draw(mode)
-    love.graphics.setColor(0, 0, 1)
-    r2:draw(mode)
-    love.graphics.setColor(1, 0.78, 0.15, 0.5)
-    love.graphics.draw(myImage, 100, 100)
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.draw(myImage, 200, 100)
+
 end
 
 function checkCollision(a, b)
@@ -55,6 +31,7 @@ function checkCollision(a, b)
         a_top < b_bottom
 end
 
+-- Error handling for the debugger
 local love_errorhandler = love.errorhandler
 
 function love.errorhandler(msg)
