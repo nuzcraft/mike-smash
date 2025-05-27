3f06d8916216573501fc2a04fc2897a121887b28
nuzcraft <20135847+nuzcraft@users.noreply.github.com>
2025-05-26 20:56:57 -0500

    finishing up panda game

--- main.lua
+++ main.lua
@@ -4,15 +4,39 @@ if arg[2] == "debug" then
 end
 
 function love.load()
-    Object = require "classic"
+    Object = require "libs.classic"
+    require "classes.player"
+    require "classes.enemy"
+    require "classes.bullet"
+    player = Player()
+    enemy = Enemy()
+    listOfBullets = {}
 end
 
 function love.update(dt)
+    player:update(dt)
+    enemy:update(dt)
 
+    for i, v in ipairs(listOfBullets) do
+        v:update(dt)
+        v:checkCollision(enemy)
+        if v.dead then
+            table.remove(listOfBullets, i)
+        end
+    end
 end
 
 function love.draw()
+    player:draw()
+    enemy:draw()
 
+    for i, v in ipairs(listOfBullets) do
+        v:draw()
+    end
+end
+
+function love.keypressed(key)
+    player:keyPressed(key)
 end
 
 function checkCollision(a, b)
